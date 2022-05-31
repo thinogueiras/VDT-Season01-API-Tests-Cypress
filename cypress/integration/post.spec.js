157d5ad33471d5e65b18b0850e857146393f0d74
thinogueiras <thiagonogueira@hotmail.com.br>
2022-05-30 23:35:23 -0300

    Adicionado o Lint e implementado o teste de POST na rota Character

---
+++ cypress/integration/post.spec.js
@@ -0,0 +1,45 @@
+const helper = require('../../cypress.json');
+
+describe('POST /characters', () => {
+    before(() => {
+        cy.request({
+            method: 'POST',
+            url: '/sessions',
+            body: {
+                email: helper.email,
+                password: helper.password,
+            },
+        }).then((response) => {
+            Cypress.env('token', response.body.token);
+        }).catch(() => {
+            cy.console();
+        });
+    });
+
+    beforeEach(() => {
+        cy.request({
+            method: 'DELETE',
+            url: `back2thepast/${helper.myApiID}`,
+        });
+    });
+
+    it('Deve cadastrar um personagem com sucesso', () => {
+        const character = {
+            name: 'Charles Xavier',
+            alias: 'Professor X',
+            team: ['x-men', 'Illuminati'],
+            active: true,
+        };
+
+        cy.request({
+            method: 'POST',
+            url: '/characters',
+            body: character,
+            headers: {
+                Authorization: Cypress.env('token'),
+            },
+        }).then((response) => {
+            expect(response.status).to.eql(201);
+        });
+    });
+});
